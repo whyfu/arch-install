76a8737e05d52e3455153e4d7fb9d7949c200311
bedant <61743384+whyfu@users.noreply.github.com>
2023-01-26 20:03:48 +0530

    rookie mistake
    may have something to do w me being a rookie fr

--- inst-gnome.sh
+++ inst-gnome.sh
@@ -127,12 +127,12 @@ echo "craptop" > /etc/hostname
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch
 systemctl disable reflector.service
 systemctl mask reflector.service
-pacman -Syu noto-fonts noto-fonts-emoji noto-fonts-cjk networkmanager \
-	gnome-shell gdm gnome-control-center eog nautilus file-roller \
-	gnome-text-editor gnome-terminal gnome-calculator gnome-calendar \
-	xdg-user-dirs-gtk wireplumber pipewire pipewire-pulse pipewire-alsa \
-	pipewire-jack chromium-wayland-vaapi libva-mesa-driver libva ffmpeg \
-	nvidia-dkms power-profiles-daemon libva-utils mesa-utils \
+pacman -Syu noto-fonts noto-fonts-emoji noto-fonts-cjk networkmanager \\
+	gnome-shell gdm gnome-control-center eog nautilus file-roller \\
+	gnome-text-editor gnome-terminal gnome-calculator gnome-calendar \\
+	xdg-user-dirs-gtk wireplumber pipewire pipewire-pulse pipewire-alsa \\
+	pipewire-jack chromium-wayland-vaapi libva-mesa-driver libva ffmpeg \\
+	nvidia-dkms power-profiles-daemon libva-utils mesa-utils \\
 	vulkan-icd-loader vulkan-tools vulkan-radeon usbutils gamemode gamescope
 
 # use iwd as networkmanager backend
