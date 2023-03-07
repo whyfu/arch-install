1847c9b53fe6f31e217bdcece8bb8612c1a81823
bedant <61743384+whyfu@users.noreply.github.com>
2023-03-07 19:15:09 +0530

    switch to arch's mesa
    Tkg updates too frequently, causing 2 hours of shader recompilation everyday

--- inst-gnome.sh
+++ inst-gnome.sh
@@ -121,10 +121,10 @@ pacman -Syu noto-fonts noto-fonts-emoji noto-fonts-cjk networkmanager \\
 	gnome-shell gdm gnome-control-center eog nautilus file-roller \\
 	gnome-text-editor gnome-terminal gnome-calculator gnome-calendar \\
 	xdg-user-dirs-gtk wireplumber pipewire pipewire-pulse pipewire-alsa \\
-	pipewire-jack chromium-wayland-vaapi mesa-tkg-git libva-mesa-driver libva ffmpeg \\
-	nvidia-dkms power-profiles-daemon libva-utils mesa-utils \\
-	vulkan-icd-loader vulkan-tools vulkan-radeon usbutils gamemode gamescope \\
-	arc-gtk-theme
+	pipewire-jack chromium-wayland-vaapi mesa libva-mesa-driver libva \\
+	nvidia-dkms power-profiles-daemon libva-utils mesa-utils ffmpeg \\
+	vulkan-icd-loader vulkan-tools vulkan-radeon usbutils gamemode \\
+	gamescope arc-gtk-theme
 
 # use iwd as networkmanager backend
 echo "[device]
